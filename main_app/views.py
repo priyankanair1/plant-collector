cb2643d2df98189e985178ec1981c35ca6b20b60
priyankanair1 <priyankanairammu90@gmail.com>
2023-07-31 15:20:58 -0700

    create plant list

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+plants = [
+  {'name': 'Maple', 'habit': 'Trees', 'description': 'Large, woody plants', 'growth': 40 },
+  {'name': 'Lilac', 'habit': 'Shrubs', 'description': 'Smaller woody plants', 'growth': 12},
+  {'name': 'Corn', 'habit': 'Grasses', 'description': 'Non-woody plants', 'growth': 5 },
+]
+
 
 # Create your views here.
+# Define the home view
+def home(request):
+    # Include an .html file extension - unlike when rendering EJS templates
+    return render(request, "home.html")
+
+def about(request):
+
+  return render(request, 'about.html')
+
+
+def plants_index(request):
+  
+  return render(request, 'plants/index.html', {
+    'plants': plants
+  })
